94b803032db15345e0def4c9fe3233ab76f2f1dd
MrTheRat <114981797+MrTheRat@users.noreply.github.com>
2024-04-03 14:06:08 +0200

    fully functional ActivityAPI
    no tests included

---
+++ DayPlannerAPI/Data/ActivityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DayPlannerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DayPlannerAPI.Data
+{
+    public class ActivityDbContext : DbContext
+    {
+        public ActivityDbContext(DbContextOptions<ActivityDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<PlannedActivity> PlannedActivities { get; set; }
+    }
+}
---
+++ DayPlannerAPI/Models/PlannedActivity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DayPlannerAPI.Models
+{
+    public class PlannedActivity
+    {
+        public int Id { get; set; }
+        public string ActivityName { get; set; }
+
+        [DisplayFormat(DataFormatString = "yyyy\\MM\\dd\\HH\\mm\\ss", ApplyFormatInEditMode = true)]
+        public DateTime StartTime { get; set; }
+
+        [DisplayFormat(DataFormatString = "yyyy\\MM\\dd\\HH\\mm\\ss", ApplyFormatInEditMode = true)]
+        public DateTime EndTime { get; set; }
+
+        // Foreign key
+        public int UserId { get; set; }
+    }
+}
